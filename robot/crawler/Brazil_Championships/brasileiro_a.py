ea738ad8e3fb4d1b7624335af82c4aa4399614f6
Vitor <vitor101bl9@gmail.com>
2022-08-20 23:24:10 -0300

    Adicionando funções de pegar estatisticas de um time e de pegar o ID de um time

--- robot/crawler/Brazil_Championships/brasileiro_a.py
+++ robot/crawler/Brazil_Championships/brasileiro_a.py
@@ -8,33 +8,104 @@ from bs4 import BeautifulSoup
 def last_results():
     """Scrape the last matches results and return it in an array."""
     URL = 'https://www.soccerstats.com/latest.asp?league=brazil'
-    page = requests.get(URL)
-    page_content = page.text
-    parsed_content = BeautifulSoup(page_content, 'html.parser')
-    match_html_tag = parsed_content.find_all('div', style='text-align:center;')
-    last_matches_results = []
-    for matches in match_html_tag:
-        last_matches_results.append(normalize('NFKD', matches.text))
-    return last_matches_results
+    try:
+        page = requests.get(URL)
+        page_content = page.text
+        parsed_content = BeautifulSoup(page_content, 'html.parser')
+        match_html_tag = parsed_content.find_all(
+            'div', style='text-align:center;'
+        )
+        last_matches_results = []
+        for matches in match_html_tag:
+            last_matches_results.append(normalize('NFKD', matches.text))
+        return last_matches_results
+    except:
+        return False
+
+
+def request_and_parse_to_object(url, protocol='GET'):
+    """Request the url endpoint and return the json parsed to python object."""
+    response = requests.request(protocol, url)
+    response_to_object = json.loads(response.text)
+    return response_to_object
 
 
 def table():
     """Scrape the current table"""
     url = 'https://api.sofascore.com/api/v1/unique-tournament/325/season/40557/standings/total'
-    response = requests.request('GET', url)
-    json_api_sofa = json.loads(response.text)
-    table = json_api_sofa['standings'][0]['rows']
-    team_array = []
-    for team in table:
-        team_stats = {
-            'team_name': team['team']['name'],
-            'team_position': team['position'],
-            'team_matches': team['matches'],
-            'team_wins': team['wins'],
-            'team_losses': team['losses'],
-            'team_draws': team['draws'],
-            'team_scores_for': team['scoresFor'],
-            'team_points': team['points'],
+    try:
+        json_api_sofa = request_and_parse_to_object(url=url, protocol='GET')
+        table = json_api_sofa['standings'][0]['rows']
+        team_array = []
+        for team in table:
+            team_stats = {
+                'team_name': team['team']['name'],
+                'team_position': team['position'],
+                'team_matches': team['matches'],
+                'team_wins': team['wins'],
+                'team_losses': team['losses'],
+                'team_draws': team['draws'],
+                'team_scores_for': team['scoresFor'],
+                'team_points': team['points'],
+            }
+            team_array.append(team_stats)
+        return team_array
+    except:
+        return False
+
+
+def get_team_id(team_name):
+    """get the ID from a specific team"""
+    lower_case_string = team_name.lower().replace(' ', '-')
+    url = 'https://api.sofascore.com/api/v1/unique-tournament/325/season/40557/standings/total'
+    try:
+        json_api_sofa = request_and_parse_to_object(url=url, protocol='GET')
+        team_list = json_api_sofa['standings'][0]['rows']
+        result = [
+            team['team']['id']
+            for team in team_list
+            if team['team']['slug'] == lower_case_string
+        ]
+        return result[0]
+    except:
+        return False
+
+
+def team_statistics(team_name):
+    """Return Team Statistics."""
+    TEAM_ID = get_team_id(team_name=team_name)
+    url = f'http://api.sofascore.com/api/v1/team/{TEAM_ID}/unique-tournament\
+/325/season/40557/statistics/overall'
+    try:
+        json_api_sofa = request_and_parse_to_object(url=url, protocol='GET')
+        statistics = json_api_sofa['statistics']
+        return statistics
+    except:
+        return False
+
+
+def team_overview(team_name):
+    """Get the overview about an specific club."""
+    TEAM_ID = get_team_id(team_name=team_name)
+    TEAM_STATS = team_statistics(team_name)
+    url = f'https://api.sofascore.com/api/v1/team/{TEAM_ID}'
+    try:
+        json_api_sofa = request_and_parse_to_object(url=url, protocol='GET')
+        team_object = json_api_sofa['team']
+        TEAM_OVERVIEW = {
+            'team_fullname': team_object['fullName'],
+            'stadium': team_object['venue']['stadium']['name'],
+            'manager': team_object['manager']['name'],
+            'city': team_object['venue']['city']['name'],
+            'goalsScored': TEAM_STATS['goalsScored'],
+            'goalsConceded': TEAM_STATS['goalsConceded'],
+            'yellowCards': TEAM_STATS['yellowCards'],
+            'redCards': TEAM_STATS['redCards'],
         }
-        team_array.append(team_stats)
-    return team_array
+        return TEAM_OVERVIEW
+    except:
+        return False
+
+
+if __name__ == '__main__':
+    print(team_statistics('atletico goianiense'))
